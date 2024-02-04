9ab8ccf75e560ac61cd33e7347c4b02ae51b9d13
Rob Brennan <rob@therobbrennan.com>
2024-02-03 16:25:37 -0800

    Consolidate NHL Edge API constants

--- R/constants.R
+++ R/constants.R
@@ -4,10 +4,8 @@ DEBUG_VERBOSE <- FALSE
 # Get today's date in the format YYYY-MM-DD
 todays_date <- format(Sys.Date(), "%Y-%m-%d")
 
+# NHL Edge API
 NHL_EDGE_API_BASE_URL <- "https://api-web.nhle.com/v1"
-
-NHL_EDGE_API_SCOREBOARD_URL <- paste0(NHL_EDGE_API_BASE_URL, "/score/now")
-# NHL_EDGE_API_SCOREBOARD_URL <- paste0(NHL_EDGE_API_BASE_URL, "/score/", todays_date)
-
+NHL_EDGE_API_SCOREBOARD_URL <- paste0(NHL_EDGE_API_BASE_URL, "/score/now") # OR ... <- paste0(NHL_EDGE_API_BASE_URL, "/score/", todays_date)
 NHL_EDGE_API_TIMEOUT_IN_SECONDS <- 10
 NHL_EDGE_API_TIMEZONE <- "America/Los_Angeles"
